0e2d38d5b8121938190d389c71b81714b17ad662
soyninagarcia91 <soyninagarcia91@gmail.com>
2025-05-31 17:24:49 -0300

    ej5tp5

---
+++ tp6/ej5 tp6/ej5 tp6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un carácter: ");
+        char caracter = Console.ReadKey().KeyChar;
+        Console.WriteLine(); // Salto de línea
+
+        if (Es_Digito(caracter))
+        {
+            Console.WriteLine("Es un dígito.");
+        }
+        else
+        {
+            Console.WriteLine("No es un dígito.");
+        }
+    }
+
+    // Función lógica que devuelve true si el carácter es un dígito
+    static bool Es_Digito(char c)
+    {
+        return char.IsDigit(c);
+    }
+}
+
